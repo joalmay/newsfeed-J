92b23270ba82236068bfc7456f2b9bf40098f1e0
joalmay <joalmay@gmail.com>
2014-02-04 19:35:07 -0700

    Get Information
    Signed-off-by: joalmay <joalmay@gmail.com>

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
 
+//Get Info
+$.ajax('https://api.parse.com/1/classes/newsitem', 
+  {
+    beforeSend: function(request) {
+      request.setRequestHeader('X-Parse-Application-Id', '89jlhiK1Rylg77vMfVcFzBMVtGrGI4ntJ5i1aNWY');
+            request.setRequestHeader('X-Parse-REST-API-Key', 'uDinDoZlF0rv4SH9bpmO3YeFQxzAHpS6wdDbqNij');
+        }
+    }
+).done(function(response) {
+	var news = response.results;
+	console.log(news);
+  //populate list here with jQuery
+
+  var article =document.createElement('div');
+  var title = document.createElement('h1');
+  var content = document.createElement('p');
+
+
+  $(title).text(news[1].title).appendTo(article);
+  $(content).text(news[1].body).appendTo(article);
+  $(article).addClass("newsitem").appendTo(".container");
+
+});
+
+
+
 });
